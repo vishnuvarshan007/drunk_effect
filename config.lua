e8bf8ea1b00592c103f8098bbed3ca37bd2e721a
vishnuvarshan007 <71885384+vishnuvarshan007@users.noreply.github.com>
2024-04-23 20:50:40 +0530

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,6 +3,6 @@ Config = {}
 
 -- Add new items here. The key is the item name, and the value is the duration of the effect in milliseconds.
 Config.Items = {
-    ['water'] = 20000,
+    ['water'] = 20000, --20seconds
     -- Add more items here as needed
 }
